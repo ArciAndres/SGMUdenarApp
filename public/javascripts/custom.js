62772b9a4dfc020e7e0c3a2193634d9a9e066b79
Andres Arciniegas <andresarciniegasmejia@outlook.com>
2017-05-31 08:56:43 -0500

    Las lecturas en vivo se hacen desde el archivo de json
    Y además se enlaza con una función paramétrica

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,4 +1,14 @@
-function getChart(url, elementID){
+quantity = [['Voltaje','V'],
+			['Corriente','A'],
+			['Potencia Activa','W'],
+			['Potencia Reactiva','VAR'],
+			['Potencia Aparente','VA'],
+			['Fase','°'],
+			['Factor de Potencia',''],
+			];
+
+
+function getStockChart(url, elementID){
 	$.getJSON(url, function (data) {
 		// Create the chart
 		Highcharts.stockChart(elementID, {
@@ -29,3 +39,57 @@ function getChart(url, elementID){
 function getJSONfile(url){
 	JSON.parse
 };
+
+
+function getLiveChart(elementID, phase,quantityID) {
+//  alert(mydata[0].temp);
+	$(document).ready(function () {
+		Highcharts.setOptions({
+			global: {
+				useUTC: false
+			}
+		});
+		$(elementID).highcharts({
+			chart: {
+				type: 'spline',
+				marginRight: 10,
+				events: {
+					load: function requestData() {
+						// set up the updating of the chart each second
+						var series = this.series[0];
+						var shift = false;
+						setInterval(function () {
+							shift = series.data.length > 20;
+								var y =  data.Fphases[0].fmeasures[quantityID].value;
+								var x =  Date.parse(data.date);
+								series.addPoint( [x,y], true, shift);
+						}, 2000);
+					}
+				}
+			},
+			title: {
+				text: quantity[quantityID][0]
+			},
+			xAxis: {
+				type: 'datetime',
+				tickPixelInterval: 150
+			},
+			yAxis: {
+				title: {
+					text: quantity[quantityID][0] +' ('+ quantity[quantityID][1] + ")"
+				}
+			},
+			tooltip: {
+				formatter: function () {
+					return '<b>' + this.series.name + '</b><br/>' +
+					Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+					Highcharts.numberFormat(this.y, 2);
+				}
+			},
+			series: [{
+				name: quantity[quantityID][0],
+				data: []
+			}]
+		});
+	});
+}
